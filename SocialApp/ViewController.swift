383521310993ee5dc6d7093e47a6d2278b6a0d33
Bold Lion <d@ds-mac-mini.local>
2018-10-02 11:29:02 +0300

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Bold Lion on 2.10.18.
+//  Copyright © 2018 Bold Lion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
